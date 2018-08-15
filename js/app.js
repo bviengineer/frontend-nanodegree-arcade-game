5358ccae480144f263b5884146c4fd8bd4f7098b
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-08-15 16:23:37 -0400

    Working collision detection function - with precision to be improved upon

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ class Contender {
         this.levelY = 83; //player incremental position as they traverse up and down the y-axis
         
         this.startPositionX = this.acrossX * 2; //202 - player's calculated starting position across x-axis
-        this.startPositionY = (this.levelY * 4) + 70; //422 - player calculated starting position across y-axis
+        this.startPositionY = (this.levelY * 4) + 60; //392 - player calculated starting position across y-axis
 
         this.x = this.startPositionX; //player start position on the x-axis passedd into the canvas
         this.y = this.startPositionY; //player start position on the y-axis passed into the canvas
@@ -22,10 +22,10 @@ class Contender {
         this.leftBoundary = this.x - this.x;
         this.bottomBoundary = this.y;
 
-        //collision boundaries
-        this.xCollisionBoundary = 3;
-        this.yCollisionBoundary = 3;
-        
+        //collision boundary
+        this.collisionHighX = 2 / 2;
+        this.collisionLowX = 0;
+
         //character or sprite image 
         this.sprite = "images/char-cat-girl.png";
 
@@ -47,16 +47,21 @@ class Contender {
         } else if(input === "down" && this.y < this.bottomBoundary){
             this.y += this.levelY;
         }
-    }
-    //checks for a collision between contender & any one enemy
+    }   
+    //checks for a collision between contender & any one enemy 
    collisionDetection(){        
         for(let i = 0; i < allEnemies.length; i++){
-            if((this.y - allEnemies[i].y) <= this.yCollisionBoundary && 
-                (allEnemies[i].acrossX + allEnemies[i].x) < this.x && (this.x + this.acrossX) < allEnemies[i]){
-                    console.log("collision");
-                    this.restartGame();                    
+            /*
+                This if statement is checking to determine if there is a collision, in the following order:
+                1. That the Y position of the player and enemy are the same
+                2. That the enemy is within the player game board boundary on the X axis(defined above)
+                3. That difference between the X position of the player and the X position of the enemy are within a specified boundary range 
+            */
+            if(this.y === allEnemies[i].y && Math.round(allEnemies[i].x) > this.leftBoundary && Math.round(allEnemies[i].x) <=  this.rightBoundary && Math.round(allEnemies[i].x) <= this.collisionHighX && Math.round(allEnemies[i].x) >= this.collisionLowX){
+                console.log("collision");
+                this.restartGame();
             }
-            // console.log(this.x, allEnemies[i].x);
+            // console.log(this.x, this.y, Math.round(allEnemies[i].x), allEnemies[i].y);
         }
     }
     //resets player position in the event of a collision
@@ -68,7 +73,6 @@ class Contender {
     }
     winGame(){
         if(this.y <= this.topBoundary){
-            console.log("you won the game");
             this.points = 100;
             score.innerHTML = "Your Score is: " + this.points;
             modal();
@@ -86,23 +90,20 @@ function modal(){
 
 //Event listeners for buttons on modal
 closeModalBtn.addEventListener("click", function(){
-    // player.x = player.startPositionX;
-    // player.y = player.startPositionY; 
     player.restartGame();
     gameModal.style.display = "none";                
 });
 
-
 //class constructor for the enemy bugs 
 class Enemy{
     constructor(x, y, rate){
-        this.x = x;
+        this.x = x - 10;
         this.y = y;
         this.rate = rate; //enemy's speed 
         this.sprite = "images/enemy-bug.png"
         this.acrossX = 101;
-        this.boundary = this.acrossX * 6;
-        this.restartPosition = this.acrossX - (this.acrossX * 5);
+        this.boundary = this.acrossX * 5;
+        this.restartPosition = this.acrossX - (this.acrossX * 2);
     }
     //update enemy locations
     update(dt){
@@ -121,33 +122,20 @@ class Enemy{
 }
  
 //creates enemy bugs
-const enemy1 = new Enemy(0, 60, 475);
-const enemy2 = new Enemy(-300, 83, 575);
-const enemy3 = new Enemy((-200*2.5), 166, 600);
-const enemy4 = new Enemy((-200*4), 249, 700);
-const enemy5 = new Enemy((-200), 249, 800);
-const enemy6 = new Enemy((-200), 265, 900);
-const enemy7 = new Enemy((-200), 90, 875);
-const enemy8 = new Enemy((-120), 10, 975);
-const enemy9 = new Enemy((-220), 90, 675);
-const enemy10 = new Enemy((-200), 100, 550);
+const enemyBug1 = new Enemy(0, 60, 100);
+const enemyBug2 = new Enemy(-300, 83, 575);
+const enemyBug3 = new Enemy((-200*2.5), 166, 600);
+const enemyBug4 = new Enemy((-200*4), 249, 700);
+const enemyBug5 = new Enemy((-200), 249, 800);
+const enemyBug6 = new Enemy((-200), 265, 900);
+const enemyBug7 = new Enemy((-200), 90, 875);
+const enemyBug8 = new Enemy((-120), 10, 975);
+const enemyBug9 = new Enemy((-220), 90, 675);
+const enemyBug10 = new Enemy((-200), 100, 550);
 
-//array for enemy bugs
-const allEnemies = [];  
-allEnemies.push(enemy1);
-
-//=======================================================================
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
+//well, with the enemyBugs variable declared at the top of the code, it erros out as undefined hmmm....so here it is...
+const allEnemies = []; 
+allEnemies.push(enemyBug1);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -161,5 +149,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-// Properties that both the Player and Enemy share
